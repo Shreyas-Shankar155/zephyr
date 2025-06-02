17041865118c9a4c6992137b42c79a2a0871ad4c
Shreyas-Shankar155 <s-shankar@ti.com>
2025-06-02 13:59:57 +0530

    Revert "Fixing length in pinctrl as per TRM description"
    This reverts commit 9eaa553e47f729bf9e9b8a48be0709d7c610c5f0.

--- soc/ti/k3/am6x/common/ctrl_partitions.c
+++ soc/ti/k3/am6x/common/ctrl_partitions.c
@@ -8,48 +8,28 @@
 #include <zephyr/sys/device_mmio.h>
 #include <zephyr/arch/common/sys_io.h>
 
-#if defined CONFIG_SOC_AM261X_R5F0_0
-#define KICK0_OFFSET     (0x02A4)
-#define KICK1_OFFSET     (0x02A8)
-#define KICK0_UNLOCK_VAL (0x83E70B13)
-#define KICK1_UNLOCK_VAL (0x95A4F1E0)
-#define KICK_LOCK_VAL    (0x0U)
-#else
 #define KICK0_OFFSET     (0x1008)
 #define KICK1_OFFSET     (0x100C)
 #define KICK0_UNLOCK_VAL (0x68EF3490U)
 #define KICK1_UNLOCK_VAL (0xD172BC5AU)
 #define KICK_LOCK_VAL    (0x0U)
-#endif
 
 #define CTRL_PARTITION_SIZE        (0x4000)
 #define CTRL_PARTITION(base, part) ((base) + (part) * CTRL_PARTITION_SIZE)
 
-#if defined CONFIG_SOC_AM6442_M4 || defined CONFIG_SOC_AM2434_M4
+#if defined CONFIG_SOC_AM6442_M4
 #define MCU_PADCFG_BASE (0x4080000)
 #elif defined CONFIG_SOC_AM6234_M4 | defined CONFIG_SOC_AM6232_M4
 #define WKUP_PADCFG_BASE (0x4080000)
-#elif defined CONFIG_SOC_AM2434_R5F0_0
-#define MCU_PADCFG_BASE  (0x4080000)
-#define MAIN_PADCFG_BASE (0xf0000)
-#elif defined CONFIG_SOC_AM261X_R5F0_0
-#define MAIN_PADCFG_BASE (0x53100000)
 #endif
 
 static const uintptr_t ctrl_partitions[] = {
-#if defined CONFIG_SOC_AM6442_M4 || defined CONFIG_SOC_AM2434_M4
+#if defined CONFIG_SOC_AM6442_M4
 	CTRL_PARTITION(MCU_PADCFG_BASE, 0),
 	CTRL_PARTITION(MCU_PADCFG_BASE, 1),
 #elif defined CONFIG_SOC_AM6234_M4 | defined CONFIG_SOC_AM6232_M4
 	CTRL_PARTITION(WKUP_PADCFG_BASE, 0),
 	CTRL_PARTITION(WKUP_PADCFG_BASE, 1),
-#elif defined CONFIG_SOC_AM2434_R5F0_0
-	CTRL_PARTITION(MAIN_PADCFG_BASE, 0),
-	CTRL_PARTITION(MAIN_PADCFG_BASE, 1),
-	CTRL_PARTITION(MCU_PADCFG_BASE, 0),
-	CTRL_PARTITION(MCU_PADCFG_BASE, 1),
-#elif defined CONFIG_SOC_AM261X_R5F0_0
-	CTRL_PARTITION(MAIN_PADCFG_BASE, 0),
 #endif
 };
 
